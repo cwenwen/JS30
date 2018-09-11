aabe39ad171116c1900a2a7828d2f5a814b4d98f
Amelie Cheng <amelie1815@gmail.com>
2018-09-11 23:22:43 +0100

    Day 2 html, css

---
+++ day_2/script.js
@@ -0,0 +1,24 @@
+// get hands on HTML
+const secHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secDegrees = ((seconds / 60) * 360) + 90;  // plus 90 because the hand starts from 90 degrees
+  secHand.style.transform = `rotate(${secDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;  // 1 min is 6 degrees
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;  // i hour is 30 degrees
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();  // execute immediately
